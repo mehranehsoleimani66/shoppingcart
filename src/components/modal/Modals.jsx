ba51e817fa277a21e6439992741f8f5039bb12ee
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-31 20:29:06 +0330

    feat : created modal component

--- src/components/modal/Modals.jsx
+++ src/components/modal/Modals.jsx
@@ -2,24 +2,25 @@ const Modals = ({ isvisible, onclose }) => {
   if (!isvisible) return null;
   return (
     <>
-      <>
-        <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-          <div className="relative w-1/3 my-6 mx-auto ">
-            <div className="border-0 rounded-lg  shadow-lg relative flex flex-col justify-center items-center text-white w-full bg-black outline-none">
-              <div className="relative p-6 flex justify-end items-center gap-3 text-white">
-                <span>سبد خرید</span>
-                <span>محصول</span>
-              </div>
-              <button
-                className="p-5 bg-white text-black text-bold  w-10 h-10 bg-transparent border border-spacing-0 border-gray-50"
-                onClick={() => onclose()}
-              >
-                close
-              </button>
+      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-1/3 h-2/3 my-6 mx-auto ">
+          <div className="border-0 rounded-lg  bg-black shadow-lg relative flex flex-col justify-center items-center text-white  outline-none">
+            <div
+              className="relative p-6 flex justify-end items-center gap-3 text-white"
+              dir="rtl"
+            >
+              <span>سبد خرید</span>
+              <span>محصول</span>
             </div>
+            <button
+              className="bg-white text-black text-bold flex justify-center  m-0 w-16 text-center rounded-md mb-5 bg-transparent "
+              onClick={() => onclose()}
+            >
+              close
+            </button>
           </div>
         </div>
-      </>
+      </div>
     </>
   );
 };
