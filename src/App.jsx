da27a01a920d7a3480e65ec64c0822733ce55c54
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-12-31 20:20:11 +0330

    feat : added  navbar and shop components and usecontext to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+// import React from "react";
+// import Navbar from "./components/Navbar";
+// import { Routes, Route } from "react-router-dom";
+// import Shop from "./pages/Shop";
+// import { CartProvider } from "./context/CartContext";
+// const App = () => {
+//   return (
+//     <div className=" flex flex-col justify-center items-center ">
+//       <CartProvider>
+//         <Navbar></Navbar>
+//         <Routes>
+//           <Route index element={<Shop />} />
+//         </Routes>
+//       </CartProvider>
+//     </div>
+//   );
+// };
+
+// export default App;
 import { Routes, Route } from "react-router-dom";
-import Shop from "./pages/Shop";
 import { CartProvider } from "./context/CartContext";
-const App = () => {
+import Navbar from "./components/Navbar";
+import Shop from "./pages/Shop";
+
+function App() {
   return (
-    <div className=" flex flex-col justify-center items-center ">
-      <CartProvider>
-        <Navbar></Navbar>
-        <Routes>
-          <Route index element={<Shop />} />
-        </Routes>
-      </CartProvider>
-    </div>
+    <CartProvider>
+      <Navbar></Navbar>
+      <Routes>
+        <Route index element={<Shop />} />
+        {/* <Route path="/success" element={<Success />} /> */}
+      </Routes>
+    </CartProvider>
   );
-};
+}
 
 export default App;
